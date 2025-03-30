425fe65df2fc4ceabb1618f257a1606a7210ee0e
Smithy <96345069+Smitthyy@users.noreply.github.com>
2025-03-30 23:05:31 +0100

    Added hunting license

--- web/js/config.js
+++ web/js/config.js
@@ -26,6 +26,12 @@ const config = {
             backgroundImage: 'https://i.ibb.co/vxvGzg1/card.png',
             prop: 'prop_franklin_dl',
         },
+        'huntinglicense': {
+            header: 'Hunting License',
+            background: '#c7ffe5',
+            backgroundImage: 'https://i.ibb.co/vxvGzg1/card.png',
+            prop: 'prop_franklin_dl',
+        },
         'lawyerpass': {
             header: 'Lawyer Pass',
             background: '#f9c491',
